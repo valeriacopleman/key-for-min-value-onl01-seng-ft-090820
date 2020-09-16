c7edcf57c3c982c82fa7da3254fa21a09c6a8c32
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-16 04:06:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,22 @@
 
 def key_for_min_value(name_hash)
   
-#new_hash.collect do |symbols, numbers|
- name_hash.reduce do |f, s| 
- f.last > s.last ? s : f 
- end
- first   
+ name_a = name_hash.to_a
 
-    
+  # Default key value
+  d_value= 1000
+  d_key= 0
 
-end
+  # Iterate new array 
+  name_a.each do |i|
+      # If current value is lower than default, change value&key 
+      if i[1] < d_value
+        d_value = i[1]
+        d_key = i[0]
+      end
+
+   end
+
+  return d_key
+
+end  
