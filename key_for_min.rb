dd150a1803d33bb60b37f2eb2564bd7891e171ab
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-16 04:00:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  i = 0 
+  if i< new_hash.length 
+    new_hash.collect do |symbols, numbers|
+    
+  i += 1    
+end
+    
 
 end
